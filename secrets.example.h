0a999fd8380c2786f3347f5ae4f5225743259deb
Lucas Carvallo <lucascarvallo333@gmail.com>
2026-06-13 19:57:38 -0300

    secrets.example.h

---
+++ secrets.example.h
@@ -0,0 +1,8 @@
+#ifndef SECRETS_H
+#define SECRETS_H
+
+constexpr char WIFI_SSID[] = "WIFI_CASA";
+constexpr char WIFI_PASSWORD[] = "WIFI_CASA_CLAVE";
+constexpr char LARAVEL_API_URL[] = "http://1.xx";
+
+#endif
