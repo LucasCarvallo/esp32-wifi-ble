a864d4e51ae9a520842804e1b8d4407cab4010a6
Lucas Carvallo <lucascarvallo333@gmail.com>
2026-06-13 23:16:23 -0300

    models.h: structs todavia no utilizados

--- models.h
+++ models.h
@@ -16,4 +16,58 @@ struct WifiDevice {
 //     int rssi;
 // };
 
+enum class ScanMode {
+    Managed,
+    Monitor
+};
+
+struct AccessPoint {
+    String ssid;
+    String bssid;
+    int rssi;
+    int channel;
+    bool hidden;
+};
+
+struct WifiClient {
+    String mac;
+    String associatedBssid;
+    int rssi;
+    int channel;
+};
+
+struct AccessPointDetection {
+    String ssid;
+    String bssid;
+    String detectorDeviceId;
+    String scanSessionId;
+    ScanMode scanMode;
+    unsigned long detectedAt;
+    int rssi;
+    int channel;
+};
+
+struct WifiClientDetection {
+    String mac;
+    String detectorDeviceId;
+    String scanSessionId;
+    ScanMode scanMode;
+    String associatedBssid;
+    unsigned long detectedAt;
+    int rssi;
+    int channel;
+};
+
+struct ScanBatchInfo { // Información general de un lote de escaneo
+    String deviceId;
+    ScanMode mode;
+    String detectionType;
+    unsigned long startedAt;
+    unsigned long endedAt;
+    int totalFound;
+};
+
+using ManagedWifiDevice = WifiDevice;
+using ManagedAccessPoint = AccessPoint;
+
 #endif
